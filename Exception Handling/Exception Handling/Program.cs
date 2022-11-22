c94b4632b1a8bfb701cb13a920b217c3404678cf
Alex22891 <103401307+Alex22891@users.noreply.github.com>
2022-11-22 13:48:02 +0900

    Add files via upload

---
+++ Exception Handling/Exception Handling/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 19, 0, 75, 52 };
+            try
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.WriteLine(arr[i] / arr[i + 1]);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine("An Exception has occurred : {0}", e.Message);
+            }
+            catch (DivideByZeroException e)
+            {
+
+                Console.WriteLine("An Exception has occurred : {0}", e.Message);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+
+                Console.WriteLine("An Exception has occurred : {0}", e.Message);
+            }
+            finally
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    Console.Write(" {0}", arr[i]);
+                }
+            }
+        }
+    }
+}
